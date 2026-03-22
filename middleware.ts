e22401bc0f3166322fab69f5196192ed675e01f7
weddingvenuespain-wvspartners <weddingvenuespain@gmail.com>
2026-03-22 22:58:03 +0100

    cambios v18

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,7 @@ export async function middleware(req: NextRequest) {
         getAll() {
           return req.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value }) => req.cookies.set(name, value))
           res = NextResponse.next({ request: req })
           cookiesToSet.forEach(({ name, value, options }) =>
